8b4c4ae90189c27fcd2bb4be185eff27141e2445
yalusis <yanislusis1@gmail.com>
2024-08-28 12:37:03 +0300

    made a bare project

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,5 @@
-module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+export const arrowParens = 'avoid';
+export const bracketSameLine = true;
+export const bracketSpacing = false;
+export const singleQuote = true;
+export const trailingComma = 'all';
