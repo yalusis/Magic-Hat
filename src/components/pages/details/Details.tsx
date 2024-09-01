8a6e648697c201be9cfad0876f6c0f043f832935
yalusis <yanislusis1@gmail.com>
2024-09-01 12:00:24 +0300

    changed Details screen

--- src/components/pages/details/Details.tsx
+++ src/components/pages/details/Details.tsx
@@ -39,14 +39,22 @@ function Details({ route, navigation }: DetailsScreenProps) {
       <View>
         {character.guessed ? (
           <>
-            <Text style={styleDetails.details}>House: {character.house}</Text>
-            <Text style={styleDetails.details}>Date of Birth: {character.dateOfBirth || 'Unknown'}</Text>
-            <Text style={styleDetails.details}>Actor: {character.actor}</Text>
-            <Text style={styleDetails.details}>Species: {character.species}</Text>
-            <Text style={styleDetails.details}>Ancestry: {character.ancestry}</Text>
-            <Text style={styleDetails.details}>Gender: {character.gender}</Text>
-            <Text style={styleDetails.details}>Eye colour: {character.eyeColour}</Text>
-            <Text style={styleDetails.details}>Hair colour: {character.hairColour}</Text>
+            {character.house && <Text style={styleDetails.details}>House: {character.house}</Text>}
+            {character.actor && <Text style={styleDetails.details}>Actor: {character.actor}</Text>}
+            {character.species && <Text style={styleDetails.details}>Species: {character.species}</Text>}
+            {character.ancestry && <Text style={styleDetails.details}>Ancestry: {character.ancestry}</Text>}
+            {character.gender && <Text style={styleDetails.details}>Gender: {character.gender}</Text>}
+            {character.eyeColour && <Text style={styleDetails.details}>Eye Colour: {character.eyeColour}</Text>}
+            {character.hairColour && <Text style={styleDetails.details}>Hair Colour: {character.hairColour}</Text>}
+            {character.hogwartsStaff !== undefined && <Text style={styleDetails.details}>Hogwarts Staff: {character.hogwartsStaff ? 'Yes' : 'No'}</Text>}
+            {character.hogwartsStudent !== undefined && <Text style={styleDetails.details}>Hogwarts Student: {character.hogwartsStudent ? 'Yes' : 'No'}</Text>}
+            {character.patronus && <Text style={styleDetails.details}>Patronus: {character.patronus}</Text>}
+            {character.wand.core && <Text style={styleDetails.details}>Wand Core: {character.wand.core}</Text>}
+            {character.wand.length !== null && <Text style={styleDetails.details}>Wand Length: {character.wand.length ? `${character.wand.length} cm` : 'Unknown'}</Text>}
+            {character.wand.wood && <Text style={styleDetails.details}>Wand Wood: {character.wand.wood}</Text>}
+            {character.wizard !== undefined && <Text style={styleDetails.details}>Wizard: {character.wizard ? 'Yes' : 'No'}</Text>}
+            {character.dateOfBirth && <Text style={styleDetails.details}>Date of Birth: {character.dateOfBirth}</Text>}
+            {character.yearOfBirth !== null && <Text style={styleDetails.details}>Year of Birth: {character.yearOfBirth ? character.yearOfBirth : 'Unknown'}</Text>}
           </>
         ) : (
           <View style={styleDetails.deniedBlock}>
